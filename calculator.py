921eb85a286196bca401ba7b0f4ee1eaf165a504
Niq <niqolas.ng@gmail.com>
2026-06-05 01:42:18 -0400

    optimizer instead finds cheapest work cost rather than cheapest level cost, pivoted current working pieces to specify work cost rather than level cost while level optimizer is being worked on

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
 from enchantment_data import ENCHANTMENTS
 def find_cost(item, sacrifice) :
-    
-    enchantment_cost = 0
-    for key in sacrifice.enchantments:
-        enchantment_cost += ENCHANTMENTS[key]["book_multiplier"] * sacrifice.enchantments[key]
-    
+    enchantment_cost = find_enchantment_cost(sacrifice)
     return item.calculate_work_penalty() + sacrifice.calculate_work_penalty() +  enchantment_cost
+
+
+def find_enchantment_cost(item):
+    enchantment_cost = 0
+    for key in item.enchantments:
+        enchantment_cost += ENCHANTMENTS[key]["book_multiplier"] * item.enchantments[key]
+    return enchantment_cost
--- optimizer.py
+++ optimizer.py
@@ -9,31 +9,56 @@ def create_heap(items):
     item_heap = []
 
     for item in items:
-        item_tuple = (item.calculate_work_penalty(),next(counter), item)
+        item_tuple = (item.calculate_work_penalty(), next(counter), item)
         item_heap.append(item_tuple)
     
     heapq.heapify(item_heap)
     return item_heap
 
+def find_cheapest_cost(items, cache):
+    if len(items) == 1:
+        return (0, [])
+    
+    current_minimum = float('inf')
+    for k in range(1, len(items)):
+        for subset in itertools.combinations(items, k):
+            left = subset
+            right = tuple(item for item in items if item not in left)
+            left_cost, left_steps = find_cheapest_cost(left, cache)
+            right_cost, right_steps = find_cheapest_cost(right, cache)
+            last_combination_cost = left_cost + right_cost
+            total = left_cost + right_cost + last_combination_cost
+            current_minimum = min(current_minimum, total)
 
-def find_cheapest(item_heap):
 
-    left_item = heapq.heappop(item_heap)
-    sac_item = heapq.heappop(item_heap)
+
+def find_cheapest_prior_work(item_heap):
+    if item_heap[0][2].name != 'book':
+        left_item = heapq.heappop(item_heap)
+        sac_item = heapq.heappop(item_heap)
+    else:
+        sac_item = heapq.heappop(item_heap)
+        left_item = heapq.heappop(item_heap)
+        
 
     print(f"Combine {left_item[2]} with {sac_item[2]}")
-    print(f"Costs {calculator.find_cost(left_item[2], sac_item[2])} levels")
-    print()
+    #level_cost = calculator.find_cost(left_item[2], sac_item[2])
+    
 
     new_item = left_item[2]
     new_item.add_enchantments(sac_item[2].enchantments)
     new_item.update_work(sac_item[2].prior_work)
+    print(f"{new_item} has {new_item.work}")
+    print()
 
     heapq.heappush(item_heap, (new_item.prior_work, next(counter), new_item))
+    #return level_cost
 
 def merge_loop(item_heap):
+    total_work = 0
     while len(item_heap) > 1:
-        find_cheapest(item_heap)
+        find_cheapest_prior_work(item_heap)
+
 
 def run_optimizer(items):
     merge_loop(create_heap(items))
